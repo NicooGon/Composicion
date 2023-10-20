33d68a8dcf12d102fc997983642934e7f55dfb22
Guillermo Mourelle <131787225+mourelleguillermo@users.noreply.github.com>
2023-10-20 09:18:58 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
 
@@ -8,6 +8,19 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"src\Program\beer.jpg");
+            FilterNegative negative = new FilterNegative();
+            FilterGreyscale greyscale = new FilterGreyscale();
+            PipeNull pipeLast = new PipeNull();
+            PipeSerial pipe2 = new PipeSerial(negative, pipeLast);
+            PipeSerial pipe1 = new PipeSerial(greyscale, pipe2);
+
+            pipe1.Send(picture);
+            pipe2.Send(picture);
+
+            PictureProvider prov = new PictureProvider();
+            IPicture pic = prov.SavePicture(picture, @"C:\Users\gmour\Documents\pic.png"); 
         }
     }
 }
